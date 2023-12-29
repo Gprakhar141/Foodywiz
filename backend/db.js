3fe33550d7cbb8901e3ba3ade41c0247f3bf5b92
Prakhar Gupta <gprakhar141@gmail.com>
2023-12-29 22:49:46 +0530

    Fetch data from db and add user schema

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,34 @@
 const mongoose = require("mongoose");
-const mongoURI =
-  "mongodb+srv://gprakhar141:gofood@cluster0.diz5xjt.mongodb.net/?retryWrites=true&w=majority";
-const mongoDB = async () => {
-  mongoose.connect(mongoURI, { useNewUrlParser: true })
+const mongoURI ="mongodb+srv://gprakhar141:gofood@cluster0.diz5xjt.mongodb.net/gofoodmern?retryWrites=true&w=majority";
 
-    .then(async () => {
-      console.log("Connected to MongoDB database");
-    })
-    .catch((err) => {
-      console.error("Connection error:", err);
-    });
+const optionsSchema = new mongoose.Schema({
+  half: String,
+  full: String
+});
+
+const itemSchema = new mongoose.Schema({
+  CategoryName: String,
+  name: String,
+  img: String,
+  options: [optionsSchema],
+  description: String
+});
+
+const mongoDB = async () => {
+  try {
+    await mongoose.connect(mongoURI);
+    console.log('Connected to MongoDB Atlas');
+
+    const FoodItem = mongoose.model('FoodItem', itemSchema, 'food_items');
+    const items = await FoodItem.find({}).exec();
+    console.log('Food Items:', items);
+    
+  } catch (error) {
+    console.error('Error connecting to MongoDB Atlas:', error);
+  } finally {
+    mongoose.disconnect(); // Close the Mongoose connection
+  }
 };
+
 
 module.exports = mongoDB;
